e9f934cde7799a9334f7129648e8ce3a8351980a
maksim-obodkov <obodkovm@gmail.com>
2020-10-28 16:56:54 +0300

    dns2

--- main/func.py
+++ main/func.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-path = r'chromedriver.exe'
+path = r'../chromedriver.exe'
 
 
-class Dnselements:
+class DnsElements:
 
     btn_sale = (By.CSS_SELECTOR, '.homepage-actions__title.ui-link')
     page_items = (By.XPATH, '//*[@id="actions-page"]/div[2]/div[2]/div[1]/div[3]/div/div[2]/a')
@@ -27,28 +27,30 @@ class Dnselements:
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(elements),
                                                     message=f'Поиск по элементу "{elements}" не дал результатов')
 
-    # поиск всплывающей корзины
-    def visible_cart(self, cart):
-        return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(cart),
-                                                    message=f'Поиск по элементу "{cart}" не дал результатов')
-
     # клик на элемент
     @staticmethod
     def click_on_element(elem):
         elem.click()
 
-    # сравнение количества товаров в корзине
-    @staticmethod
-    def count(number):
-        assert '1' == number, f'Количество товаров не равно "{number}"'
-
+    # сравнение данной ссылки со ссылкой на текущую страницу
     def link_check(self, link):
         assert link == self.driver.current_url, f'Мы не перешли на "{link}"'
 
+    # выбор нужного атрибута из ссылки на текущую страницу
     def segment_link(self, string):
         link = self.driver.current_url
         link = link.split('/')
         assert string == link[-2], f'Мы не перешли на "{string}"'
+
+    # поиск всплывающей корзины
+    def visible_cart(self, cart):
+        return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(cart),
+                                                    message=f'Поиск по элементу "{cart}" не дал результатов')
+
+    # сравнение количества товаров в корзине
+    @staticmethod
+    def count(number):
+        assert '1' == number, f'Количество товаров не равно "{number}"'
 
     def quit(self):
         self.driver.quit()
