1273cd210df0aa731a2a5ccaa0d7f27b7ebcfc41
maksim-obodkov <obodkovm@gmail.com>
2020-11-15 18:50:42 +0300

    test01

--- tests/tests.py
+++ tests/tests.py
@@ -18,9 +18,9 @@ class Test01:
         assert 'https://www.dns-shop.ru/actions/' in self.driver.current_url, f'Мы не перешли на страницу'
         # проверяем соовпадение ссылок по атрибуту
         assert self.sales_page.first_link() == self.sales_page.segment_link(), f'Мы не перешли на страницу'
-        # клик по кнопке "купить"
+        # клик по кнопке «купить»
         self.product_page.find_button_buy()
         # проверка появление высплывающего окна
         assert self.product_page.visible_cart() == True, f'Окно с товарами в корзине не появилось'
-        # сравнение количества товаров в корзине с числом "1"
+        # сравнение количества товаров в корзине с числом «1»
         assert '1' == self.product_page.count() , f'Число товаров в корзине не равно 1'
