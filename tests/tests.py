bdf903948785f3989f6a4057aebc705262f450b0
maksim-obodkov <obodkovm@gmail.com>
2020-11-15 18:48:03 +0300

    комменты

--- tests/tests.py
+++ tests/tests.py
@@ -12,10 +12,15 @@ class Test01:
         self.index_page = IndexPage(self.driver)
         self.sales_page = SalesPage(self.driver)
         self.product_page = ProductPage(self.driver)
+        # открываем страницу
         self.index_page.go_to_page()
-        assert 'https://www.dns-shop.ru/actions/' in self.driver.current_url, f'Мы не перешли на страницу' #доделать
+        # проверяем по ссылкам
+        assert 'https://www.dns-shop.ru/actions/' in self.driver.current_url, f'Мы не перешли на страницу'
+        # проверяем соовпадение ссылок по атрибуту
         assert self.sales_page.first_link() == self.sales_page.segment_link(), f'Мы не перешли на страницу'
+        # клик по кнопке "купить"
         self.product_page.find_button_buy()
+        # проверка появление высплывающего окна
         assert self.product_page.visible_cart() == True, f'Окно с товарами в корзине не появилось'
+        # сравнение количества товаров в корзине с числом "1"
         assert '1' == self.product_page.count() , f'Число товаров в корзине не равно 1'
-        print('все окей')
